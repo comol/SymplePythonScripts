718f01467db1397b60c43b4628ecd3ddee903003
comol <comol@mail.ru>
2020-02-27 02:19:24 +0300

    initial commit

---
+++ ConvertTif.py
@@ -0,0 +1,19 @@
+﻿import os
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = 1000000000  
+
+path = u'Path to file dir' # поменяем директорию на ту, где у нас расположены картинки
+
+print(path)
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+     for file in f:
+        if ('.tif' in file or '.tiff' in file or '.dib' in file) and (not '.png' in file):            
+            fullpath = os.path.join(r, file)
+            if not os.path.exists(fullpath + ".png"):
+                try:                    
+                    im = Image.open(fullpath)
+                    im.save(fullpath + ".png", "PNG")
+                except:
+                    print("error converting file " + fullpath)
---
+++ WatchChanges.py
@@ -0,0 +1,39 @@
+﻿import os
+import sys
+import time
+from PIL import Image
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+Image.MAX_IMAGE_PIXELS = 1000000000  
+
+path = u'path to file watches' # поменяем директорию на ту, где у нас расположены картинки
+
+class Event(LoggingEventHandler):
+    def dispatch(self, event):
+        if event.event_type == 'modified':
+            if event.is_directory == False:
+                if ('.tif' in event.src_path or '.tiff' in event.src_path) and (not '.png' in event.src_path):                    					
+					self.convertfile(event.src_path)
+    
+    def convertfile(self, filename):
+        time.sleep(10) 
+        if not os.path.exists(filename + ".png"):
+                try:                    
+                    im = Image.open(filename)
+                    im.save(filename + ".png", "PNG")
+                except:
+                    print("error converting file " + filename)
+
+
+event_handler = Event()
+observer = Observer()
+observer.schedule(event_handler, path, recursive=True)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
